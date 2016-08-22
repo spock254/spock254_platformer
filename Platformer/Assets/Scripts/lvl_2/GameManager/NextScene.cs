7f7762d0f2dab76de967c4f1c1391de7d0be2ebc
DESKTOP-GSLHTUE\Vika&Sasha <qwertyspock254@gmail.com>
2016-08-22 15:48:56 +0300

    add first simple scene changer system

---
+++ Platformer/Assets/Scripts/lvl_2/GameManager/NextScene.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextScene : MonoBehaviour {
+
+    private SceneController sceneController;
+    private Transform playerTransform;
+    private float distance = 0f;
+
+    void Start() {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        sceneController = SceneController.GetSceneController;
+    }
+    void Update() {
+        distance = Vector3.Distance(playerTransform.position, this.transform.position);
+        if (distance < 1.5) {
+            sceneController.ChangeScene(1);
+        }
+    }
+
+}
---
+++ Platformer/Assets/Scripts/lvl_2/GameManager/SceneController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+    
+    private SceneController() { }
+
+    private static SceneController sceneController;
+
+    public static SceneController GetSceneController {
+        get {
+            if (!sceneController) {
+                sceneController = GameObject.FindGameObjectWithTag("PlayerManager").GetComponent<SceneController>();
+                return sceneController;
+            }
+            return sceneController;
+        }
+    }
+    public void ChangeScene(int scene_index) {
+        if (SceneManager.sceneCount <= scene_index)
+            SceneManager.LoadScene(scene_index);
+        else
+            throw new System.Exception("scene index more then max scene count");   
+    }
+    public void ChangeScene(string scene_name)
+    {
+            SceneManager.LoadScene(scene_name);
+    }
+
+}
